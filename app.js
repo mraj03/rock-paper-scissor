bddd5e6437e40c5d7f48cf019b3b366d0c743e3c
Melvin Raj <melvinraj0004@gmail.com>
2023-12-05 10:30:03 +0530

    MODIFIED Index.html and App.js
    Modified the  app.js function to have the game running perfectly. Need to restructure code.

--- app.js
+++ app.js
@@ -1,77 +1,60 @@
+let playerScore = 0;
+let computerScore = 0;
 
+const getComputerChoice = () => {
+    const choiceArr = ['rock', 'paper', 'scissor'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    const computerChoice = choiceArr[randomNumber];
 
-function getComputerChoice(){   
-    let moves = ['rock', 'paper', 'scissor'];
-    let computerMove = moves[Math.floor(Math.random() * moves.length)]
-    return computerMove;
+    return computerChoice;
 }
 
-function humanPlay() {
-    humanChoice = prompt('Please choose and write from "Rock", "Paper" or "Scissor"').toLowerCase();
-    return humanChoice;
-}
-
-
-function playRound (playerSelection, computerSelection) {
-
-    playerSelection = humanPlay();
-    computerSelection = getComputerChoice();
-    console.log('Your selection is: ' + playerSelection);
-    console.log('Computer selection is: ' + computerSelection);
-
-    switch (playerSelection + computerSelection) {
-        case 'rockrock':
-        case 'paperpaper':
-        case 'scissorscissor':
-            return 'Tie. Both show same value.'; 
-            break;
-        case 'rockpaper':
-        case 'paperscissor':
-        case 'scissorrock':
-            return "Computer wins";
-            break;
-        case 'rockscissor':
-        case 'paperrock':
-        case 'scissorpaper':
-            return "You win";
-            break;
+const playRound = (playerSelection, computerSelection) => {
+    if(playerSelection === 'rock' && computerSelection === 'rock'){
+        return 'You tied! you both picked rock';
+    } else if(playerSelection === 'paper' && computerSelection === 'paper'){
+        return 'You tied! you both picked paper';
+    } else if(playerSelection === 'scissor' && computerSelection === 'scissor'){
+        return 'You tied! you both picked scissor';
+    } else if(playerSelection === 'rock' && computerSelection === 'paper'){
+        computerScore++;
+        return 'You lost! paper covers rock';
+    } else if(playerSelection === 'rock' && computerSelection === 'scissor'){
+        playerScore++;
+        return 'You win! rock beats paper';
+    } else if(playerSelection === 'paper' && computerSelection === 'scissor'){
+        computerScore++;
+        return 'You lost! scissor cuts paper';
+    } else if(playerSelection === 'paper' && computerSelection === 'rock'){
+        playerScore++;
+        return 'You win! paper covers rock';
+    } else if(playerSelection === 'scissor' && computerSelection === 'paper'){
+        playerScore++;
+        return 'You win! scissor cuts paper';
+    } else if(playerSelection === 'scissor' && computerSelection === 'rock'){
+        computerScore++;
+        return 'You lost! rocks beats paper';
     }
-}
+} 
 
-
-
-function game (numberRounds) {
-    let humanWins = 0;
-    let computerWins = 0;
-    let Draw = 0;
-
-    for (let i = 0; i < numberRounds; i++){
-        round = i + 1;
-        
-        let gameResults = playRound(humanPlay(), getComputerChoice());
-        console.log("Round "+ round + "\n" + "Human plays " + humanPlay() + " : Computer plays " + getComputerChoice()  + " \n" + gameResults );
-    }
-    
-    if (gameResults === "Human wins") {
-        humanWins++;
-    }
-    else if (gameResults === "Computer wins") {
-        computerWins++;
-    }
-    else if (gameResults === "Draw") {
-        Draw++;
+const game = () => {
+    for(let i = 0; i < 5; i++){
+        const playerSelection = prompt('type your choice', 'Select either rock, or paper, or scissor').toLowerCase();
+        const computerSelection = getComputerChoice();
+        console.log(`1. player selection: `, `${playerSelection} `, `2. computer  selection:: `, `${computerSelection}`); 
+        playRound(playerSelection, computerSelection);
     }
 
-    if (humanWins > computerWins) {
-        return ("Human Wins " + humanWins + " - " + computerWins )
-    }
-    else if (computerWins > humanWins) {
-        return ("Computer Wins " + computerWins + " - " + humanWins)
-    }
-    else {
-        return ("Match Drawn - Play again")
+    if (playerScore > computerScore){
+        return 'You won, great job winning against the computer.';
+    } else if(computerScore > playerScore){
+        return `You lost! don't lose hope you need one win`;
+    } else {
+        return 'You tied with the computer. Hey, better than losing to it.'
     }
 }
 
-console.log(game(5));
 
+
+
+console.log(game());
